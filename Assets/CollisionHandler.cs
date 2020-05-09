4c6268947ab64b3c293b06a724817c28b1966d64
Alex Chernov <alexusblack@gmail.com>
2020-05-09 15:49:07 +1000

    previnting excess energy creation

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -4,99 +4,83 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private static bool coroutineActive = false;
-    private static List<Rigidbody2D> atoms = new List<Rigidbody2D>();
-    private Rigidbody2D rb2d;
-    private bool primaryCollider = false;
-    //private float collisionTotalMagnitude = 0;
-    
-    public float lastMagnitude = 0;
+  // Flag so that coroutine would be launched only once
+  private static bool coroutineActive = false;
+  // List of all atoms in the simulation
+  private static List<Rigidbody2D> atoms = new List<Rigidbody2D>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-      rb2d = GetComponent<Rigidbody2D>();
-      atoms.Add(rb2d);
+  // Rigidbody component cache
+  private Rigidbody2D rb2d;
+  
+  // Record of last magnitude
+  public float lastMagnitude = 0;
 
-      if(!coroutineActive) {
-        coroutineActive = true;
-        StartCoroutine(CalculateTKEDaemon(1.0f));
-      }
+  // Start is called before the first frame update
+  void Start()
+  {
+    // Caching components
+    rb2d = GetComponent<Rigidbody2D>();
+    atoms.Add(rb2d);
+
+    // Launching coroutine if it isn't running yet
+    if(!coroutineActive) {
+      coroutineActive = true;
+      StartCoroutine(CalculateTKEDaemon(1.0f));
     }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-      if(lastMagnitude == 0) lastMagnitude = rb2d.velocity.magnitude;
+  // Total Kinetic Energy Caclulation demon
+  IEnumerator CalculateTKEDaemon(float waitTime) {
+    while(true) {
+      yield return new WaitForSeconds(waitTime);
+      Debug.Log("T.K.E. = " + CalculateTKE());
     }
+  }
 
-    IEnumerator CalculateTKEDaemon(float waitTime) {
-      while(true) {
-        yield return new WaitForSeconds(waitTime);
-        Debug.Log("T.K.E. = " + CalculateTKE());
-      }
+  // Method for Total Kinetic Energy calculation
+  float CalculateTKE() {
+    float tke = 0;
+    foreach(var atom in atoms) {
+      tke += atom.velocity.magnitude;
     }
+    return tke;
+  }
 
-    float CalculateTKE() {
-      float tke = 0;
-      foreach(var atom in atoms) {
-        tke += atom.velocity.magnitude;
-      }
-      return tke;
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    // Updating recorded velocity magnitude
+    lastMagnitude = rb2d.velocity.magnitude;
+  }
+
+  // Monitoring all colisions
+  void OnCollisionEnter2D(Collision2D col) {
+    // Participant with highest magnitude will handle the collision
+    if(rb2d.velocity.magnitude > col.rigidbody.velocity.magnitude) {
+      // Retriving other CollisionHandler
+      CollisionHandler otherCollisionHandler = col.gameObject.GetComponent<CollisionHandler>();
+      // Extracting its last magnitude value
+      float otherLastMagnitude = otherCollisionHandler ? otherCollisionHandler.lastMagnitude : 0;
+      //Debug.Log("1: " + lastMagnitude + " 2: " + otherLastMagnitude);
+      //Debug.Log("1: " + rb2d.velocity.magnitude + " 2: " + col.rigidbody.velocity.magnitude);
+
+      // Calculating total velocity magnitude before and after the collision
+      float collisionTotalMagnitude = lastMagnitude + otherLastMagnitude;
+      float newCollisionTotalMagnitude = rb2d.velocity.magnitude + col.rigidbody.velocity.magnitude;
+
+      // If new total velocity magnitude gain excess energy clamping it to stay on previous level proportionately
+      if(newCollisionTotalMagnitude > collisionTotalMagnitude) {
+        // calculating clamping proportions
+        float ratio1 = rb2d.velocity.magnitude / newCollisionTotalMagnitude;
+        float ratio2 = col.rigidbody.velocity.magnitude / newCollisionTotalMagnitude;
+
+        // clamping veloicty values
+        rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, collisionTotalMagnitude * ratio1);
+        // don't try to clamp walls velocity
+        if(otherLastMagnitude != 0) col.rigidbody.velocity = Vector2.ClampMagnitude(col.rigidbody.velocity, collisionTotalMagnitude * ratio2);
+
+        //Debug.Log("1: " + rb2d.velocity.magnitude + " 2: " + col.rigidbody.velocity.magnitude);
+      } 
     }
-
-    void OnCollisionEnter2D(Collision2D col) {
-      //Debug.Log("OnCollisionEnter2D");
-      if(rb2d.velocity.magnitude > col.rigidbody.velocity.magnitude) {
-        
-        CollisionHandler otherCollisionHandler = col.gameObject.GetComponent<CollisionHandler>();
-        float otherLastMagnitude = otherCollisionHandler ? otherCollisionHandler.lastMagnitude : 0;
-        Debug.Log("1: " + lastMagnitude + " 2: " + otherLastMagnitude);
-        Debug.Log("1: " + rb2d.velocity.magnitude + " 2: " + col.rigidbody.velocity.magnitude);
-
-        float collisionTotalMagnitude = lastMagnitude + otherLastMagnitude;
-        float newCollisionTotalMagnitude = rb2d.velocity.magnitude + col.rigidbody.velocity.magnitude;
-
-        if(newCollisionTotalMagnitude > collisionTotalMagnitude) {
-          float ratio1 = rb2d.velocity.magnitude / newCollisionTotalMagnitude;
-          float ratio2 = col.rigidbody.velocity.magnitude / newCollisionTotalMagnitude;
-
-          rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, collisionTotalMagnitude * ratio1);
-          col.rigidbody.velocity = Vector2.ClampMagnitude(col.rigidbody.velocity, collisionTotalMagnitude * ratio2);
-
-          Debug.Log("1: " + rb2d.velocity.magnitude + " 2: " + col.rigidbody.velocity.magnitude);
-        }
-        lastMagnitude = rb2d.velocity.magnitude;
-        if(otherCollisionHandler) otherCollisionHandler.lastMagnitude = col.rigidbody.velocity.magnitude; 
-      }
-    }
-
-    // void OnCollisionExit2D(Collision2D col) {
-    //   if(primaryCollider) {
-    //     primaryCollider = false;
-    //     float newCollisionTotalMagnitude = rb2d.velocity.magnitude + col.rigidbody.velocity.magnitude;
-    //     float keLoss = newCollisionTotalMagnitude - collisionTotalMagnitude;
-
-    //     Debug.Log("1: " + rb2d.velocity.magnitude + " 2: " + col.rigidbody.velocity.magnitude);
-    //     //Debug.Log("K.E.Loss = " + keLoss);
-
-    //     if(keLoss > 0) {
-    //       //Debug.Log(name + " - " + col.gameObject.name + " - " + keLoss);
-    //       float ratio1 = rb2d.velocity.magnitude / newCollisionTotalMagnitude;
-    //       float ratio2 = col.rigidbody.velocity.magnitude / newCollisionTotalMagnitude;
-
-    //       // TODO: clamp magnitude proportionaly to keep K.E. same
-    //       Debug.Log(rb2d.velocity + " -> " + collisionTotalMagnitude * ratio1);
-    //       Debug.Log(col.rigidbody.velocity + " -> " + collisionTotalMagnitude * ratio2);
-    //       rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, collisionTotalMagnitude * ratio1);
-    //       col.rigidbody.velocity = Vector2.ClampMagnitude(col.rigidbody.velocity, collisionTotalMagnitude * ratio2);
-    //       
-    //       //Debug.Break();
-    //     }
-    //     //totalKE -= keLoss;
-    //     //Debug.Log("K.E.Loss: " + keLoss);
-    //     //if(keLoss > 1) Debug.Break();
-    //     //if(keLoss > 1) Debug.Log("T.K.E. = " + totalKE);
-    //   }
-    // }
+  }
 }
